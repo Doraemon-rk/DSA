a8d385224db1f949f3b35ef20842b1b0deec7444
Doraemon-rk <arpitakumaripatwa@gmail.com>
2025-08-18 17:27:08 +0530

    Sum of Rows wise element in 2D Matrix / Array

---
+++ 2D_Matrix 3.cpp
@@ -0,0 +1,37 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // sum of row wise in 2d array/matrix
+
+    int arr[4][4];
+
+    // taking input in an 2d array
+
+    cout << "enter the array elements" << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+
+            cin >> arr[i][j];
+        }
+    }
+
+    // row-wise sum ka logic
+    for(int i=0; i<4; i++){
+
+        int sum=0;
+
+        for(int j=0; j<4; j++){
+
+           sum = sum + arr[i][j];
+        }
+
+        cout << "sum of row -: " i << " " << sum << endl;
+    }
+
+   
+} 
